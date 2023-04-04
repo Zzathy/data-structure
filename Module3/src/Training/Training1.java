157e0c13b3e2d29077bdd0f4ace097a30e5d4cd8
Izza Ihsan Fathony <izzafathony27@gmail.com>
2023-04-04 23:16:32 +0700

    Training on module 3

---
+++ Module3/src/Training/Training1.java
@@ -0,0 +1,25 @@
+package Training;
+
+import java.util.Stack;
+
+public class Training1 {
+    public static void main(String[] args) {
+        Stack st = new Stack();
+        st.push("Aku");
+        st.push("Anak");
+        st.push("Indoensia");
+
+        System.out.println("Next : " + st.peek());
+        st.push("Raya");
+        System.out.println(st.pop());
+        st.push("!");
+
+        int count = st.search("Aku");
+        while (count != -1 && count > 1) {
+            st.pop();
+            count--;
+        }
+        System.out.println(st.pop());
+        System.out.println(st.empty());
+    }
+}
---
+++ Module3/src/Training/Training2.java
@@ -0,0 +1,19 @@
+package Training;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Training2 {
+    public static void main(String[] args) {
+        Queue queue = new LinkedList();
+        queue.add("Java");
+        queue.add("DotNet");
+        queue.add("PHP");
+        queue.add("HTML");
+        System.out.println("Remove : " + queue.remove());
+        System.out.println("Element : " + queue.element());
+        System.out.println("poll : " + queue.poll());
+        System.out.println("Peek : " + queue.peek());
+
+    }
+}
